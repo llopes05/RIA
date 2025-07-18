47457cf0e6d9875da27844514de34c88b09da80c
Luiz Felipe <167254490+llopes05@users.noreply.github.com>
2025-07-17 22:38:43 -0300

    chore: comentário do caminho da rota

--- projetoRIA/src/app/app.routes.ts
+++ projetoRIA/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-    { path: 'animes', loadComponent: () => import('./pages/anime/anime.component').then(m => m.AnimeComponent) },
+    { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }, // Read, Update, Delete
+    { path: 'animes', loadComponent: () => import('./pages/anime/anime.component').then(m => m.AnimeComponent) }, // Create
     { path: '**', redirectTo: 'home' }
 ];
